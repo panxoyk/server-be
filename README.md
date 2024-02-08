38709777bcfa189fe1ab262b3a61c6cea4cfe5e8
panxoyk <francisco.cstllhrnndz@gmail.com>
2024-02-08 17:01:29 -0300

    Server del Login con datos del formulario en el header.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usuario
+
+<francisco.cstllhrnndz@gmail.com>
+11111 -> $2b$10$368z.lpViFu.tUBkwiHs9O9sx73X416yOTcBRviGK1AKeP32uBlNa
---
+++ src/index.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { Schema, model } from "mongoose"
+import { connect } from 'mongoose'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.route('/')
+    .get((req, res) => {
+        res.send('Welcome to server')
+    })
+
+app.route('/login')
+    .post(async (req, res) => {
+        const { email, password } = JSON.parse(req.headers.login)
+        const user = await UserModel.findOne({ email })
+        if (!user) return
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) return
+        const session = jwt.sign({ email }, "holasecreto777")
+        res.send({ session })
+    })
+
+app.listen(port, async () => {
+    console.log(`Server running on port ${port}`)
+    await connect('mongodb+srv://Francisco:Fach0018@cluster0.xqzrlqv.mongodb.net/server-be?retryWrites=true&w=majority')
+    console.log('Connected to MongoDB')
+})
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { collection: "users" })
+
+UserSchema.set('toJSON', {
+    transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const UserModel = model("User", UserSchema)
